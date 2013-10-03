85bec5b06d6e59bb58a0f34d5d0edc61ceb9af07
Alexander Longerbone <long0385@bebop.morris.umn.edu>
2013-10-03 15:47:03 -0500

    made some progress on file_disemvowel

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -1,12 +1,14 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<ctype.h>
+#include <unistd.h>
 
-int is_vowel(char c){
+int is_vowel(char c) {
   /*Returns true if c is a vowel (upper or lower case), and false otherwise.*/
 
   char lower = tolower(c);
 
-  if (lower != "a" && lower != "e" && lower != "i" && lower != "o" && lower != "u") {
+  if (lower != 'a' && lower != 'e' && lower != 'i' && lower != 'o' && lower != 'u') {
     return 0;
   } else {
     return 1;
@@ -22,17 +24,33 @@ copied over*/
   int i = 0;
   int non_vowels = 0;
   for (i; i < num_chars; i++) {
-    if(!isvowel(in_buf[i]) {
+    if(is_vowel(in_buf[i]) == 0) {
 	out_buf[i] = in_buf[i];
 	non_vowels++;
-      } else {
-	break;
-      }
-      return non_vowels;
+    } else {
+      break;
+    }
+    printf("%d \n", non_vowels);
+}
+
+void disemvowel(FILE* inputFile, FILE* outputFile) {
+  /* 
+   * Copy all the non-vowels from inputFile to outputFile.
+   * Create input and output buffers, and use fread() to repeatedly read
+   * in a buffer of data, copy the non-vowels to the output buffer, and
+   * use fwrite to write that out.
+   */
+
+  
 }
 
 int main(int argc, char **argv){
+  FILE *inputFile;
+  FILE *outputFile;
 
-  copy_non_vowels(5, {"H", "e", "l", "l", "o"}, {});
-  
+  /* Code that processes the command line arguments and sets up inputFile and outputFile */
+
+  disemvowel(inputFile, outputFile);
+
+  return 0;
 }
